2f8f2c0657cd2a8c4b4ac19a66a5bc7745c4ab56
saeka8 <156178882+saeka8@users.noreply.github.com>
2024-12-02 15:59:08 +0100

    views.py

--- group_final/weather_data/weather/views.py
+++ group_final/weather_data/weather/views.py
@@ -1,15 +1,13 @@
-import json
-import matplotlib
+import matplotlib,urllib, base64
 matplotlib.use('Agg')
+import pandas as pd
 import matplotlib.pyplot as plt
-import io
-import urllib, base64
+from io import BytesIO
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.core.serializers.json import DjangoJSONEncoder
 from .models import WeatherWeather, WeatherCity, WeatherCountry
 from .forms import WeatherForm 
-import pandas as pd
 
 
 def all_weather(request):
@@ -35,19 +33,18 @@ def generate_graph(dates, temperatures, humidities, title, xlabel):
 
     ax1.set_xlabel(xlabel)
     ax1.set_ylabel('Temperature (°F)', color='r')
-    ax1.plot(dates, temperatures, color='r', label='Temperature')
+    ax1.plot(dates, temperatures, 'r-')
     ax1.tick_params(axis='y', labelcolor='r')
-    ax1.set_ylim(0, 100)
+    ax1.set_ylim(0,100)
 
     ax2 = ax1.twinx()
     ax2.set_ylabel('Humidity (%)', color='b')
-    ax2.plot(dates, humidities, color='b', label='Humidity')
+    ax2.plot(dates, humidities, 'b-')
     ax2.tick_params(axis='y', labelcolor='b')
-    ax2.set_ylim(0, 100)
+    ax2.set_ylim(0,100)
 
-    fig.tight_layout()
     plt.title(title)
-    buf = io.BytesIO()
+    buf = BytesIO()
     plt.savefig(buf, format='png')
     buf.seek(0)
     string = base64.b64encode(buf.read())
@@ -63,22 +60,14 @@ def city_weather(request, city_id):
     df['date'] = pd.to_datetime(df['date'])
     df.set_index('date', inplace=True)
 
-    # Average temperature and humidity graph
-    monthly_avg = df.resample('M').mean()
+    # Calculate monthly averages across all years
+    monthly_avg = df.groupby(df.index.month).mean()
+    monthly_avg.index = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     average_graph = generate_graph(monthly_avg.index, monthly_avg['temperature'], monthly_avg['humidity'], 'Average Temperature and Humidity Per Month', 'Month')
-
-    # Yearly temperature and humidity graphs
-    year_graphs = {}
-    for year in range(2015, 2024):
-        yearly_data = df[df.index.year == year]
-        if not yearly_data.empty:
-            year_graphs[year] = generate_graph(yearly_data.index, yearly_data['temperature'], yearly_data['humidity'], f'Temperature and Humidity in {year}', 'Date')
 
     return render(request, 'city_weather.html', {
         'weather_data': weather_data,
         'average_graph': average_graph,
-        'year2016_graph': year_graphs.get(2016, ''),
-        'year2017_graph': year_graphs.get(2017, ''),
         
     })
 
@@ -92,4 +81,5 @@ def create_weather(request):
     else:
         form = WeatherForm()
     return render(request, 'create_weather.html', {'form': form})
+
 
